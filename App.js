85832a1c12923735f3be755f9949565b7752ff33
nhope <nvevincye@gmail.com>
2021-02-10 15:41:56 -0500

    button layout

--- App.js
+++ App.js
@@ -12,38 +12,38 @@ import {
 
 class Calculator extends Component {
   render(){
+    // Declare variables
     const numbers = [[9,8,7],[6,5,4],[3,2,1],['.',0,'=']];
-    const operators = ['/','*','-','+'];
+    const operators = ['C','Del','/','*','-','+'];
     let numberButtons = [];
     let operatorButtons = [];
+    // Operator buttons
     operators.forEach((item, i) => {
       operatorButtons.push(
-        <TouchableOpacity >
+        <TouchableOpacity key={'operators' + i} style={styles.operatorBtn}>
           <Text>{item}</Text>
         </TouchableOpacity >)
     });
-
-    for (var i = 0; i < numbers.length; i++) {
-      var constest = [];
-      console.log(i);
-      for (var j = 0; j < numbers[i].length; j++) {
-        constest.push(
-          <TouchableOpacity style={styles.digits} >
-            <Text>{numbers[i][j]}</Text>
+    // Digits Buttons
+    numbers.forEach((item, i) => {
+      var content = [];
+      item.forEach((value, u) => {
+        content.push(
+          <TouchableOpacity key={'digits' +i + u} style={styles.digits} >
+            <Text>{value}</Text>
           </TouchableOpacity >)
-
-      }
-
-      numberButtons.push(<View style={styles.numberRows}>{constest}</View>);
-    }
-
+      });
+      numberButtons.push(<View style={styles.numberRows}>{content}</View>);
+    });
 
 
 
     return (
       <View style={styles.container} >
-        <View style={styles.display} > 123*313.0-1023</View>
-        <View  style={styles.results}>12</View>
+        <View style={[styles.display, styles.rightText]} >
+         <Text>{'123*313.0-1023'}</Text>
+        </View>
+        <View  style={[styles.results, styles.rightText]}><Text>12</Text></View>
         <View  style={styles.buttons}>
           {/* Numbers Buttons */}
           <View style={styles.numbers}>
@@ -71,19 +71,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontSize: 28,
+  },
+  rightText:{
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    padding: 15,
   },
   display:{
     flex: 2,
     backgroundColor: 'red',
+
   },
   results:{
     flex: 1,
-    background-color: 'blue',
+    backgroundColor: 'blue',
+
   },
   buttons:{
     flex: 7,
     backgroundColor: 'green',
     flexDirection: 'row',
+    fontWeight: 'bold',
   },
   numbers:{
     flex: 4,
@@ -101,7 +110,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-
+    borderRadius: 20,
+  },
+  operatorBtn:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 
 });
